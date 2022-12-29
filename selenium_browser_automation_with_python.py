16b9b9962780787a453feeaa2c60ea93e07be3eb
pfulcher26 <pfulcher26@gmail.com>
2022-12-29 12:21:01 -0800

    Adds an option to leave the browser window open once a task is completed.

--- selenium_browser_automation_with_python.py
+++ selenium_browser_automation_with_python.py
@@ -3,4 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
+options = Options()
+options.add_experimental_option("detach", True)
+
 driver = webdriver.Chrome(service=Service(ChromeDriverManager.install()))
