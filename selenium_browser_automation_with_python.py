88c87abf6c705bf99ced298fec710891bb15589f
pfulcher26 <pfulcher26@gmail.com>
2022-12-29 12:24:03 -0800

    Adds code to navigate to the website nueralnine.com

--- selenium_browser_automation_with_python.py
+++ selenium_browser_automation_with_python.py
@@ -6,4 +6,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 options = Options()
 options.add_experimental_option("detach", True)
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager.install()))
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://www.neuralnine.com/")
